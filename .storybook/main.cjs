d66223643b682540ebf344837f871ae427276bfe
Samanta Fluture <samantafluture@gmail.com>
2022-10-12 22:23:35 -0400

    ci: Add legacy to npm ci script

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
 	},
 	viteFinal: (config, { configType }) => {
 		if (configType === 'PRODUCTION') {
-			config.base = 'react-design-system-storybook'
+			config.base = '/react-design-system-storybook'
 		}
 	},
 }
